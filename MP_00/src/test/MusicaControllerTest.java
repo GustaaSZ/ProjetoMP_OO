83a5a8cbbc5fa792d5ba5ff44d210270eb2c911b
ArthurGabriel-Sortech <arthurelgg@gmail.com>
2023-01-20 09:19:28 -0300

    Melhorando testes

--- MP_00/src/test/MusicaControllerTest.java
+++ MP_00/src/test/MusicaControllerTest.java
@@ -1,8 +1,9 @@
-/*package test;
+package test;
 
 import main.controller.ArtistaController;
 import main.controller.MusicaController;
 import main.model.Musica;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -11,12 +12,21 @@ import static main.util.Instanciacao.instanciar;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MusicaControllerTest{
+
+    @BeforeEach
+    void setUp() {
+        instanciar();
+    }
+
     @Test
     void buscarMusicaPorNome() {
-        instanciar();
-        String nome = "exemplo musica nao cadastrada";
-        Musica result = MusicaController.buscarMusicaPorNome(nome);
-        assertNull(result);
+        String musicaNaoCadastrada = "Musica nao cadastrada";
+        Musica result1 = MusicaController.buscarMusicaPorNome(musicaNaoCadastrada);
+        assertNull(result1);
+
+        String musicaCadastrada = "Quieto";
+        Musica result2 = MusicaController.buscarMusicaPorNome(musicaCadastrada);
+        assertEquals(musicaCadastrada, result2.getNome());
     }
 
     @Test
@@ -29,9 +39,11 @@ class MusicaControllerTest{
 
     @Test
     void adicionarArtista() {
-        instanciar();
         MusicaController musicaController = new MusicaController(MusicaController.buscarMusicaPorNome("Quieto"));
-        boolean result = musicaController.adicionarArtista(ArtistaController.buscarArtistaPorNome("Leviano"));
-        assertFalse(result);
+        boolean result1 = musicaController.adicionarArtista(ArtistaController.buscarArtistaPorNome("Leviano"));
+        assertFalse(result1);
+
+        boolean result2 = musicaController.adicionarArtista(ArtistaController.buscarArtistaPorNome("Veigh"));
+        assertTrue(result2);
     }
-}*/
+}
